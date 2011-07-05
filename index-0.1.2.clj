aea50af8739e7cf058a634cebfe22abe7538c101
Tom Faulhaber <git_net@infolace.com>
2011-07-04 23:46:57 -0700

    Initial Documentation Commit

---
+++ index-0.1.2.clj
@@ -0,0 +1,114 @@
+{:namespaces
+ ({:source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath/clojure.java.classpath-api.html",
+   :name "clojure.java.classpath",
+   :author "Stuart Sierra",
+   :doc "Utilities for dealing with the JVM's classpath"}),
+ :vars
+ ({:arglists ([]),
+   :name "classpath",
+   :namespace "clojure.java.classpath",
+   :source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj#L58",
+   :raw-source-url
+   "https://github.com/clojure/java.classpath/raw/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath//clojure.java.classpath-api.html#clojure.java.classpath/classpath",
+   :doc
+   "Returns a sequence of File objects of the elements on the\nclasspath.",
+   :var-type "function",
+   :line 58,
+   :file
+   "/home/tom/src/clj/autodoc/../autodoc-work-area/java.classpath/src/src/main/clojure/clojure/java/classpath.clj"}
+  {:arglists ([]),
+   :name "classpath-directories",
+   :namespace "clojure.java.classpath",
+   :source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj#L64",
+   :raw-source-url
+   "https://github.com/clojure/java.classpath/raw/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath//clojure.java.classpath-api.html#clojure.java.classpath/classpath-directories",
+   :doc
+   "Returns a sequence of File objects for the directories on classpath.",
+   :var-type "function",
+   :line 64,
+   :file
+   "/home/tom/src/clj/autodoc/../autodoc-work-area/java.classpath/src/src/main/clojure/clojure/java/classpath.clj"}
+  {:arglists ([]),
+   :name "classpath-jarfiles",
+   :namespace "clojure.java.classpath",
+   :source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj#L69",
+   :raw-source-url
+   "https://github.com/clojure/java.classpath/raw/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath//clojure.java.classpath-api.html#clojure.java.classpath/classpath-jarfiles",
+   :doc
+   "Returns a sequence of JarFile objects for the JAR files on classpath.",
+   :var-type "function",
+   :line 69,
+   :file
+   "/home/tom/src/clj/autodoc/../autodoc-work-area/java.classpath/src/src/main/clojure/clojure/java/classpath.clj"}
+  {:arglists ([]),
+   :name "clojure-classpath",
+   :namespace "clojure.java.classpath",
+   :source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj#L51",
+   :raw-source-url
+   "https://github.com/clojure/java.classpath/raw/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath//clojure.java.classpath-api.html#clojure.java.classpath/clojure-classpath",
+   :doc
+   "Returns a sequence of File paths from Clojure's base classloader.",
+   :var-type "function",
+   :line 51,
+   :file
+   "/home/tom/src/clj/autodoc/../autodoc-work-area/java.classpath/src/src/main/clojure/clojure/java/classpath.clj"}
+  {:arglists ([jar-file]),
+   :name "filenames-in-jar",
+   :namespace "clojure.java.classpath",
+   :source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj#L35",
+   :raw-source-url
+   "https://github.com/clojure/java.classpath/raw/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath//clojure.java.classpath-api.html#clojure.java.classpath/filenames-in-jar",
+   :doc
+   "Returns a sequence of Strings naming the non-directory entries in\nthe JAR file.",
+   :var-type "function",
+   :line 35,
+   :file
+   "/home/tom/src/clj/autodoc/../autodoc-work-area/java.classpath/src/src/main/clojure/clojure/java/classpath.clj"}
+  {:arglists ([f]),
+   :name "jar-file?",
+   :namespace "clojure.java.classpath",
+   :source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj#L27",
+   :raw-source-url
+   "https://github.com/clojure/java.classpath/raw/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath//clojure.java.classpath-api.html#clojure.java.classpath/jar-file?",
+   :doc
+   "Returns true if file is a normal file with a .jar or .JAR extension.",
+   :var-type "function",
+   :line 27,
+   :file
+   "/home/tom/src/clj/autodoc/../autodoc-work-area/java.classpath/src/src/main/clojure/clojure/java/classpath.clj"}
+  {:arglists ([]),
+   :name "system-classpath",
+   :namespace "clojure.java.classpath",
+   :source-url
+   "https://github.com/clojure/java.classpath/blob/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj#L43",
+   :raw-source-url
+   "https://github.com/clojure/java.classpath/raw/9e70cab246f21caf93ac13b12cd50a79540122ad/src/main/clojure/clojure/java/classpath.clj",
+   :wiki-url
+   "http://clojure.github.com/java.classpath//clojure.java.classpath-api.html#clojure.java.classpath/system-classpath",
+   :doc
+   "Returns a sequence of File paths from the 'java.class.path' system\nproperty.",
+   :var-type "function",
+   :line 43,
+   :file
+   "/home/tom/src/clj/autodoc/../autodoc-work-area/java.classpath/src/src/main/clojure/clojure/java/classpath.clj"})}
